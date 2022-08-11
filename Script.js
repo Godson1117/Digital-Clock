2880d2bc6a9a3f1e0e63c56cf64e7674cbcd9405
Godson Sojan <91582855+Godson1117@users.noreply.github.com>
2022-08-11 17:34:41 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,6 +5,7 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 btn.addEventListener('click',()=> {
+  alert("Hello...")
   setInterval(()=> {
   let t=new Date()
   let element=`<h4 class="text-danger mx-auto" style="font-size:2rem;text-shadow:0px 2px 1px white">${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}</h4><br><h5 class="text-info mx-auto" style="text-shadow:2px 0px 1px white"><i>${days[t.getDay()]} , ${months[t.getMonth()]} ${t.getDate()} ${t.getFullYear()}</i></h5>`
